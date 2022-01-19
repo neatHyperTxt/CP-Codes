369960d006bd08201288d6ccd0d5e578deabdc3a
neatHyperTxt <94314268+neatHyperTxt@users.noreply.github.com>
2022-01-19 16:58:26 +0530

    Update Euclid's_gcd1.cpp

--- GCD/Euclid's_gcd1.cpp
+++ GCD/Euclid's_gcd1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#define ll long long int
 
+using namespace std;
+
+
+ll gcd(ll a,ll b)
+{
+	return (b?gcd(b,a%b):a);
+}
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	ll a,b;
+	cout<<"Enter the First Number"<<endl;
+	cin>>a;
+	cout<<"Enter the Second Number"<<endl;
+	cin>>b;
+	cout<<gcd(a,b)<<endl;
+	return 0;
+}
